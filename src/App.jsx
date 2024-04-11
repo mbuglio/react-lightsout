928acf79f85a89ce2900edbfb5b749de35411105
Megan Buglio <meganbuglio@Megans-MacBook-Pro.local>
2024-04-11 14:26:41 -0500

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import React from "react";
+import Board from "./Board";
+import "./App.css";
+
+/** Simple app that just shows the LightsOut game. */
+
+function App() {
+  return (
+      <div className="App">
+        <Board />
+      </div>
+  );
+}
+
+export default App;
+
